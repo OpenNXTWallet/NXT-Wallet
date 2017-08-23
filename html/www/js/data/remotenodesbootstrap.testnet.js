d46480d3c3a83d497efe4e72f39d920ea016a80b
Sven <sven.barthel@gmail.com>
2017-08-23 18:38:39 +0200

    version 1.11.8

---
+++ html/www/js/data/remotenodesbootstrap.testnet.js
@@ -0,0 +1,158 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016-2017 Jelurida IP B.V.                                     *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of the Nxt software, including this file, may be copied, modified, *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+    {
+        "peers": [
+            {
+                "hallmark": "112e0c5748b5ea610a44a09b1ad0d2bddc945a6ef5edc7551b80576249ba585b0c003130372e3137302e332e363201000000a37b3301c01c388286fb5407792481f4ff85d44f286457b85686d7e1cc71f33a4482aad20762153584538cb36ff7f10d5a2a392bfb2434eafe3e867afee780618d4362be42",
+                "downloadedVolume": 25618025,
+                "address": "107.170.3.62",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 11805418,
+                "uploadedVolume": 24102198,
+                "services": [
+                    "HALLMARK",
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.7",
+                "platform": "unix",
+                "inboundWebSocket": true,
+                "lastUpdated": 117834201,
+                "blacklisted": false,
+                "announcedAddress": "107.170.3.62",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 117808985,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "hallmark": "7f377944d8d45a1deeaa8496efce37eb43c817d6423a2a6f7639d049d0af530a0b006e78742e6e6f69702e6d65640000008a2d3301ef78f2f627f533804665692d3309f47571e22d47597e6ccdd6a865583495d55b09e37161d1aede6d3257ad61c7dc3ed413c0325c6e311a1f4e4b5666b30b25d257",
+                "downloadedVolume": 10086929,
+                "address": "178.150.207.53",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 2101,
+                "uploadedVolume": 9994579,
+                "services": [
+                    "HALLMARK",
+                    "PRUNABLE",
+                    "API",
+                    "API_SSL",
+                    "CORS"
+                ],
+                "version": "1.11.7",
+                "platform": "nxt.noip.me",
+                "inboundWebSocket": true,
+                "apiSSLPort": 6877,
+                "lastUpdated": 117831321,
+                "blacklisted": false,
+                "announcedAddress": "nxt.noip.me",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 117831321,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "hallmark": "dc7b03d7dd03fe316bb3321c65683c7e528dbed6bdedd67d5aeae2e3dd1701260f003137342e3134302e3136382e31333601000000a17b3301b82ca4f88c519b9ed19e150140e58641aaf81593e740e2b7473b8fec07d390c704760e425f3cc04903da7e39d9590cde5bc04ac609a9cc648e53e54dc37ff37317",
+                "downloadedVolume": 11580803,
+                "address": "174.140.168.136",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 183391,
+                "uploadedVolume": 14399244,
+                "services": [
+                    "HALLMARK",
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.7",
+                "platform": "512VPS",
+                "inboundWebSocket": true,
+                "lastUpdated": 117832441,
+                "blacklisted": false,
+                "announcedAddress": "174.140.168.136",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 117803596,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "downloadedVolume": 9813483,
+                "address": "5.200.23.15",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 9798252,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.6",
+                "platform": "Linux amd64",
+                "inboundWebSocket": false,
+                "lastUpdated": 117833170,
+                "blacklisted": false,
+                "announcedAddress": "5.200.23.15",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 117829558,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "hallmark": "209f0babc2bfeaecaa312d5573034ab8274a450959d28fbf86b607d37f73fa4514006272616e67646f6e2e6475636b646e732e6f72670100000013c73301108ace189df5121ef61a34fcf2446ab386b39b98172166526b5ea6d9316a3f9108b0063c220bec362431be1a2e14b49c05e5f9543a3e709de631cd87904df7bc73",
+                "downloadedVolume": 11203989,
+                "address": "82.9.0.245",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 1008818,
+                "uploadedVolume": 10902049,
+                "services": [
+                    "HALLMARK",
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.7",
+                "platform": "Brangdon2",
+                "inboundWebSocket": true,
+                "lastUpdated": 117831948,
+                "blacklisted": false,
+                "announcedAddress": "brangdon.duckdns.org",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 117828345,
+                "state": 1,
+                "shareAddress": true
+            }
+        ],
+        "requestProcessingTime": 24
+    };
